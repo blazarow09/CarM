ea74fe6cdb6b65d2c090d74a70bd431484ed0668
blazarow <blazarow09@gmail.com>
2020-07-25 20:41:29 +0300

    added home button and home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,61 @@
 import * as React from 'react';
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonIcon,
+    IonTitle,
+    IonContent,
+} from '@ionic/react';
+import { settingsOutline as settingsIcon, exitOutline as exitIcon} from 'ionicons/icons';
+import { IUserStore } from '../../stores/UserStore/UserStore';
+import { observer, inject } from 'mobx-react';
 
-export default class Home extends React.Component {
+interface HomeProps {
+    userStore?: IUserStore;
+}
 
-  public render() {
-    return (
-     <p>Works</p>
-    );
-  }
+@inject('userStore')
+@observer
+export default class Home extends React.Component<HomeProps> {
+    private toggleTheme(checked: boolean): void {
+        if (!checked) {
+            document.body.classList.toggle('dark', checked);
+        } else {
+            document.body.classList.toggle('dark', checked);
+        }
+    }
+
+    private async logout (): Promise<void> {
+        await this.props.userStore.handleLogout();
+
+    }
+
+    public render() {        
+        return (
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar color="secondary">
+                        <IonButtons slot="end">
+                            <IonButton>
+                                <IonIcon icon={settingsIcon} slot="icon-only" />
+                            </IonButton>
+                            <IonButton onClick={async (): Promise<void> => await this.logout()}>
+                                <IonIcon icon={exitIcon} slot="icon-only" />
+                            </IonButton>
+                        </IonButtons>
+                        <IonTitle>Home</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent>
+                    {/* <IonItem>
+                        <IonLabel>Checked:</IonLabel>
+                        <IonToggle onIonChange={(e) => this.toggleTheme(e.detail.checked)} />
+                    </IonItem> */}
+                </IonContent>
+            </IonPage>
+        );
+    }
 }
---
+++ src/components/Spinners/Spinners.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { IonSpinner } from '@ionic/react';
+
+export const MainSpinner: React.FC = () => {
+    return <IonSpinner name="crescent" />;
+};
+
+
