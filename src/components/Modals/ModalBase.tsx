ac3acf86a65d1e7cf7511d955bee15e7ce51dc88
blazarow <blazarow09@gmail.com>
2020-08-01 20:10:05 +0300

    fixed android back button event when in modal

--- src/components/Modals/ModalBase.tsx
+++ src/components/Modals/ModalBase.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { IonModal } from '@ionic/react';
 import { IUiStore } from '../../stores/UiStore/UiStore';
 import MainHeader from '../MainHeader/MainHeader';
+import './ModalBase.css';
 
 export interface IModalBaseProps {
     // onDidDismiss?: = (): void};
     swipeToClose?: boolean;
-
     uiStore?: IUiStore;
 }
 
@@ -16,6 +16,18 @@ export interface IModalBaseState {
 }
 
 export default class ModalBase<TProps extends IModalBaseProps, TState extends IModalBaseState> extends React.Component<TProps, TState> {
+    private backButtonListener = (_event: any): any => {
+        this.props.uiStore.closeAllModals();
+    };
+
+    public componentDidMount(): void {
+        document.addEventListener('ionBackButton', this.backButtonListener, true);
+    }
+
+    componentWillUnmount(): void {
+        document.removeEventListener('ionBackButton', this.backButtonListener, true);
+    }
+
     protected visible(): boolean {
         return false;
     }
@@ -51,8 +63,11 @@ export default class ModalBase<TProps extends IModalBaseProps, TState extends IM
                 swipeToClose={this.props.swipeToClose ? this.props.swipeToClose : false}
                 // onDidDismiss={(): void => this.props.onDidDismiss()}
             >
-                <MainHeader extraContent={this.extraContent} title={this.state?.headerTitle} toolbarColor={this.state?.headerToolbarColor} />
-
+                <MainHeader
+                    extraContent={this.extraContent}
+                    title={this.state?.headerTitle}
+                    toolbarColor={this.state?.headerToolbarColor}
+                />
                 {this.content()}
             </IonModal>
         );
