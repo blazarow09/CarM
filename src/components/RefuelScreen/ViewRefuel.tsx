ac52721e372c26a4a1dd4406e7ddce7dba9a2343
blazarow <blazarow09@gmail.com>
2020-08-20 21:24:59 +0300

    Enhanced view refuel

--- src/components/RefuelScreen/ViewRefuel.tsx
+++ src/components/RefuelScreen/ViewRefuel.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import MainHeader from '../MainHeader/MainHeader';
 import { GlobalColors } from '../../models/Constants/GlobalColors';
-import { IonPage, IonContent, IonButtons, IonBackButton, IonItem, IonLabel } from '@ionic/react';
+import { IonPage, IonContent, IonButtons, IonBackButton, IonItem, IonLabel, IonIcon, IonRow, IonCol } from '@ionic/react';
 import { AppRoutes } from '../AppRoutes';
 import { IVehicleStore } from '../../stores/VehicleStore/VehicleStore';
 import { observer, inject } from 'mobx-react';
 import { IUserStore } from '../../stores/UserStore/UserStore';
+import { calendarOutline as dateIcon, chevronBackOutline as backIcon, chevronForwardOutline as nextIcon } from 'ionicons/icons';
+import './ViewRefuel.css';
 
 interface ViewRefuelProps {
     vehicleStore?: IVehicleStore;
@@ -29,11 +31,24 @@ export default class ViewRefuel extends React.Component<ViewRefuelProps> {
             <IonPage>
                 <MainHeader toolbarColor={GlobalColors.purpleColor} title="Refuel details" extraContent={this.extraContent} />
                 <IonContent>
-                    <IonItem>
-                        <IonLabel>{this.props?.vehicleStore.viewRefuelData?.totalCost}</IonLabel>
-                        <IonLabel>{this.props?.vehicleStore.viewRefuelData?.quantity}</IonLabel>
-                        <IonLabel>{this.props?.vehicleStore.viewRefuelData?.pricePerLtr}</IonLabel>
-                        <IonLabel>{this.props?.vehicleStore.viewRefuelData?.mileage}</IonLabel>
+                    <IonItem className="c-background-tab" lines="none">
+                        <IonRow className="c-date-content">
+                            <IonCol size="2" className="c-button-click-effect" ion-align-items-start>
+                                <IonIcon icon={backIcon} onClick={() => console.log('previous')} />
+                            </IonCol>
+                            <IonCol size="8" className="c-date-info">
+                                <IonIcon icon={dateIcon} size="small" />
+                                <span>19 Aug 2020 - 19:45</span>
+                            </IonCol>
+                            <IonCol size="2" ion-align-items-end className="c-button-click-effect">
+                                <IonIcon icon={nextIcon} />
+                            </IonCol>
+                        </IonRow>
+                    </IonItem>
+                    <IonItem className="c-background-tab c-space-between" lines="none">
+                        <IonRow className="">
+
+                        </IonRow>
                     </IonItem>
                 </IonContent>
             </IonPage>
