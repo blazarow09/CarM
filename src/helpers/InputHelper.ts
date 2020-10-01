a58c80195379763518163e5f67d46e1151456204
blazarow <blazarow09@gmail.com>
2020-10-01 22:52:30 +0300

    enhanced add vehicle input fields, added more options

---
+++ src/helpers/InputHelper.ts
@@ -0,0 +1,24 @@
+export class InputHelper {
+    static fuelTypes = [
+        {
+            value: 'CNG',
+            label: 'CNG',
+        },
+        {
+            value: 'Diesel',
+            label: 'Diesel',
+        },
+        {
+            value: 'Gasoline',
+            label: 'Gasoline',
+        },
+        {
+            value: 'LPG',
+            label: 'LPG',
+        },
+        {
+            value: 'Electric',
+            label: 'Electric',
+        },
+    ];
+}
